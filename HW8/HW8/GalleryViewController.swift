6fa1055a4f699242c96a6cb69dafd8ff18833bcd
Tatiana <73175008+TatyanaV24@users.noreply.github.com>
2020-11-25 14:42:02 +0300

    Изменения в GalleryViewController
    Создала модель, как Вы предложили. Однако, так и не смогла придумать, как именно выводить наименования в UILabel. Пыталась, как и с картинками, так и с UILabel вывести наименование, но всеопять начинает прыгать. Надеюсь поделитесь подсказкой, как лучше исправить этот недочет.

--- HW8/HW8/GalleryViewController.swift
+++ HW8/HW8/GalleryViewController.swift
@@ -6,86 +6,47 @@
 //
 
 import UIKit
-
+struct Cat {
+    let icon: UIImage
+    let name: String
+}
 class GalleryViewController: UIViewController {
-
+    var coordinateX = 0
+    var coordinateY = 0
+    var stepY = 0
+    var definitionOfPositonImage = 0
     let catsImage = [UIImage(named:"1"), UIImage(named:"2"),UIImage(named:"3"),UIImage(named:"4")]
-    
+    let catsName = ["Funny cat","Cute cat", "Smile rabbit", "Happy cat"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let imageLabel0 = UILabel()
-        let imageLabel1 = UILabel()
-        let imageLabel2 = UILabel()
-        let imageLabel3 = UILabel()
-        
-        imageLabel0.text = "Funny cat"
-        imageLabel1.text = "Cute cat"
-        imageLabel2.text = "Sad cat"
-        imageLabel3.text = "Good cat"
-        
-       // let imageLabelArr = [imageLabel0, imageLabel1, imageLabel2, imageLabel3]
-        
-        
- //задала размер ImageView
-        let screenWidth = Double(self.view.frame.size.width)
-        let imageWidth = screenWidth / 2 - 10 - 10
-        let imageHeight = imageWidth * 1.5
-        
- //инициализация переменных для изображения координат
-        var coordinateX = 0
-        var coordinateY = 0
-        var stepY = 0
-        var definitionOfPositonImage = 0
-        
         for i in 0...catsImage.count - 1 {
-           // for j in 0...imageLabelArr.count - 1 {
             if i % 2 == 0 && definitionOfPositonImage % 2 == 0  {
-                
-                //установить значение координаты
                 coordinateX = 15
                 stepY += 1
-                
             } else {
-                
                 coordinateX = 220
-                
             }
-            //сдвиг картинки вниз по Y
-            definitionOfPositonImage += 5
+            definitionOfPositonImage += 4
             coordinateY = stepY * 200
             
+            let oneView = UIView(frame: CGRect(x: coordinateX, y:coordinateY, width: 174, height: 275))
+            oneView.backgroundColor = UIColor.systemGray6
+            let oneImage = UIImageView(frame: CGRect(x: 0, y: 0, width: 174, height: 245))
+            let oneLabel = UILabel(frame: CGRect(x: 0, y: 245, width: 174, height: 30))
+            let modelCat = Cat(icon: catsImage[i]!, name: catsName[1])
+            oneImage.image = modelCat.icon
+            oneLabel.text = modelCat.name
             
-            let imageView = UIImageView()
-            imageView.image = catsImage[i]
-            var imageLabel = UILabel()
-            //imageLabel = imageLabelArr[j]
-            
-            
-            //чтобы первые две картинки не смещались по Y
             if i == 0 || i == 1 {
-                
-                //задание положения и размера картинки
-                imageView.frame = CGRect(x: coordinateX, y: 50, width: Int(imageWidth), height: Int(imageHeight))
-                
-                imageLabel.frame = CGRect(x: coordinateX, y: 350, width: Int(imageWidth), height: 30)
-                imageLabel.backgroundColor = UIColor.gray
-                //imageLabel.text = "Funny cat's"
-                
+                oneView.frame = CGRect(x: coordinateX, y:56, width: 174, height: 275)
             } else {
-                
-                imageView.frame = CGRect(x: coordinateX, y: coordinateY, width: Int(imageWidth), height: Int(imageHeight))
-                imageLabel.frame = CGRect(x: coordinateX, y: 700, width: Int(imageWidth), height: 30)
-                imageLabel.backgroundColor = UIColor.gray
-                //imageLabel.text = "Cute cat's"
+                oneView.frame = CGRect(x: coordinateX, y:coordinateY, width: 174, height: 275)
             }
-            
-            view.addSubview(imageView)
-            view.addSubview(imageLabel)
+            view.addSubview(oneView)
+            oneView.addSubview(oneImage)
+            oneView.addSubview(oneLabel)
         }
     }
-    }
-
-
-
+}
