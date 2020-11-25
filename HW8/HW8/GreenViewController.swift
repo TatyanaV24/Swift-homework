f5617ba9c7cce39007cf9d91db5ddcc10e0427f7
Tatiana <73175008+TatyanaV24@users.noreply.github.com>
2020-11-25 14:51:00 +0300

    Добавила доп.задачу в ветку
    Задача решена не полностью.
    Хочу получить Ваши комментарии, т.к. сейчас уперлась в стену и не понимаю, чего именно не замечаю.
    С учетом условия задачи, рассуждаю так:
    1. Пользователь наживает на кнопку "1", кнопка изменяет цвет и открывается первый контроллер.
    2. Пользователь наживает на кнопку "2", кнопка изменяет цвет и открывается второй контроллер. При этом цвет кнопки "1" должен вернуться к первоначальному, а первый контроллер должен быть скрыт.
    3. Пользователь наживает на кнопку "3", кнопка изменяет цвет и появляются два контрролера. При этом у кнопки "1" и "2" цвет сохраняется прежний.
    Мои кнопки меняют цвет и выводят контроллеры, но мне так и не удалось научиться скрывать нужный контроллер и возвращать кнопкам цветам.
    У меня такое ощущение, что я некорректно поняла условие задачи. Буду признательна за комментарии с Вашей стороны, чтобы двигаться дальше.
    Спасибо

---
+++ HW8/HW8/GreenViewController.swift
@@ -0,0 +1,17 @@
+//
+//  GreenViewController.swift
+//  HW8
+//
+//  Created by Татьяна Васильченко on 25.11.2020.
+//
+
+import UIKit
+
+class GreenViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = UIColor.systemGreen
+        view.frame = CGRect(x: 2, y: 100, width: 411, height: 378)
+    }
+}
---
+++ HW8/HW8/YellowViewController.swift
@@ -0,0 +1,17 @@
+//
+//  YellowViewController.swift
+//  HW8
+//
+//  Created by Татьяна Васильченко on 25.11.2020.
+//
+
+import UIKit
+
+class YellowViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = UIColor.systemYellow
+        view.frame = CGRect(x: 2, y: 476, width: 411, height: 378)
+    }
+}
