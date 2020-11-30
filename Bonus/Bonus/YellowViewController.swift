eec9f3a0bc1087c0f5bcf0b569635defe19aafb5
Tatiana <73175008+TatyanaV24@users.noreply.github.com>
2020-11-30 12:56:08 +0300

    Дополнительная задача
    Дополнительную задачу вынесла в отдельную ветку и отдельный проект.

---
+++ Bonus/Bonus/YellowViewController.swift
@@ -0,0 +1,17 @@
+//
+//  YellowViewController.swift
+//  Bonus
+//
+//  Created by Татьяна Васильченко on 30.11.2020.
+//
+
+import UIKit
+
+class YellowViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = UIColor.systemYellow
+        view.frame = CGRect(x: 2, y: 476, width: 411, height: 378)
+    }
+}
