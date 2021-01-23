65c3f0f2d8b3077685f6c2b6b74535adb64983af
Tatiana <73175008+TatyanaV24@users.noreply.github.com>
2021-01-23 20:37:37 +0300

    измнения в таблице
    Я дополнила таблицу, она грузится практически верно.
    У меня остались вопросы еще по двум элемента:
    1. Для добавлению в строку "Wi-Fi" названия сети, должен создаваться еще лейбл, или есть функция, которая позволяет добавлять текст.
    2. Также есть строка "Основные" на скрине есть оповещение ввиде красного кружка. Тут тоже должна быть функция, или он должен добавляться в ранее созданную структуру.
    Спасибо.

--- HW10/HW10/SetTableViewCell.swift
+++ HW10/HW10/SetTableViewCell.swift
@@ -12,16 +12,4 @@ class SetTableViewCell: UITableViewCell {
     @IBOutlet weak var iconImage: UIImageView!
     @IBOutlet weak var nameLabel: UILabel!
     @IBOutlet weak var switchButton: UISwitch!
-    
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
-    }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
-    }
-
 }
--- HW10/HW10/TableViewController.swift
+++ HW10/HW10/TableViewController.swift
@@ -22,48 +22,53 @@ struct SettingSection {
     let title: String
     let settings: [Setting]
 }
-class SettingsViewController: UIViewController {
-    let settingSections = [
-        SettingSection(
-            title: "1",
-            settings: [Setting(title: "Авиарежим", icon: UIImage(named: "1-1"), isSwitchHidden: true),
-                       Setting(title: "WI-FI", icon: UIImage(named: "1-2"), isSwitchHidden: false),
-                       Setting(title: "Bluetooth", icon: UIImage(named: "1-3"), isSwitchHidden: false),
-                       Setting(title: "Сотовая связь", icon: UIImage(named: "1-4"), isSwitchHidden: false),
-                       Setting(title: "Режим модема", icon: UIImage(named: "1-5"), isSwitchHidden: false)]),
-        SettingSection(
-            title: "2",
-            settings: [Setting(title: "Уведомления", icon: UIImage(named: "1-7"), isSwitchHidden: false),
-                       Setting(title: "Звуки, тактильные сигналы", icon: UIImage(named: "1-1"), isSwitchHidden: false),
-                       Setting(title: "Не беспокоить", icon: UIImage(named: "1-8"), isSwitchHidden: false),
-                       Setting(title: "Экранное время", icon: UIImage(named: "1-9"), isSwitchHidden: false)]),
-        SettingSection(
-            title: "3",
-            settings: [Setting(title: "Основаные", icon: UIImage(named: "1-10"), isSwitchHidden: false),
-                       Setting(title: "Пункт управления", icon: UIImage(named: "1-11"), isSwitchHidden: false),
-                       Setting(title: "Экран и яркость", icon: UIImage(named: "1-12"), isSwitchHidden: false)])]
-}
 
 class TableViewController: UIViewController {
     
-    var cont: SettingsViewController!
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-    }
+    let settingSections = [
+        SettingSection(
+            title: " ",
+            settings: [Setting(title: "Авиарежим", icon: UIImage(named: "1-1"), isSwitchHidden: false),
+                       Setting(title: "WI-FI", icon: UIImage(named: "1-2"), isSwitchHidden: true),
+                       Setting(title: "Bluetooth", icon: UIImage(named: "1-3"), isSwitchHidden: true),
+                       Setting(title: "Сотовая связь", icon: UIImage(named: "1-4"), isSwitchHidden: true),
+                       Setting(title: "Режим модема", icon: UIImage(named: "1-5"), isSwitchHidden: true)]),
+        SettingSection(
+            title: " ",
+            settings: [Setting(title: "Уведомления", icon: UIImage(named: "1-6"), isSwitchHidden: true),
+                       Setting(title: "Звуки, тактильные сигналы", icon: UIImage(named: "1-7"), isSwitchHidden: true),
+                       Setting(title: "Не беспокоить", icon: UIImage(named: "1-8"), isSwitchHidden: true),
+                       Setting(title: "Экранное время", icon: UIImage(named: "1-9"), isSwitchHidden: true)]),
+        SettingSection(
+            title: " ",
+            settings: [Setting(title: "Основаные", icon: UIImage(named: "1-10"), isSwitchHidden: true),
+                       Setting(title: "Пункт управления", icon: UIImage(named: "1-11"), isSwitchHidden: true),
+                       Setting(title: "Экран и яркость", icon: UIImage(named: "1-12"), isSwitchHidden: true)])]
+
 }
 extension TableViewController: UITableViewDataSource, UITableViewDelegate {
     
     func numberOfSections(in tableView: UITableView) -> Int {
-        return cont.settingSections.count
+        return settingSections.count
     }
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return cont.settingSections[section].settings.count
+        
+        return settingSections[section].settings.count
     }
-    
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return settingSections[section].title
+    }
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "setCell") as! SetTableViewCell
-        cell.nameLabel.text = cont.settingSections[indexPath.row].title
+        let cell = tableView.dequeueReusableCell(withIdentifier: "setCell",for: indexPath) as! SetTableViewCell
+        let name = settingSections[indexPath.section].settings[indexPath.row]
+        cell.nameLabel.text = name.title
+        cell.iconImage.image = name.icon
+        cell.switchButton.isHidden = name.isSwitchHidden
+        if indexPath.section == 0, indexPath.row == 0 {
+            cell.accessoryType = .none
+        } else {
+            cell.accessoryType = .disclosureIndicator
+        }
         return cell
     }
 }
