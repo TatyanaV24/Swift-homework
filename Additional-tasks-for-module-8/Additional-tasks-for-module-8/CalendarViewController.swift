55278a3d21267eaa18649fb204469e4d3505203a
Tatiana <73175008+TatyanaV24@users.noreply.github.com>
2021-02-23 20:18:22 +0300

    заключительные правки
    Внесла необходимые изменения. Сейчас все работает верно.

--- Additional-tasks-for-module-8/Additional-tasks-for-module-8/CalendarViewController.swift
+++ Additional-tasks-for-module-8/Additional-tasks-for-module-8/CalendarViewController.swift
@@ -13,19 +13,16 @@ class CalendarViewController: UIViewController {
     @IBOutlet weak var calendarDate: UIDatePicker!
     @IBOutlet weak var newTimeTextField: UITextField!
     @IBOutlet weak var dateLabel: UILabel!
-    var timeZone = [-1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
     
-    @IBAction func changeLabel(_ sender: UIDatePicker) {
+    @IBAction func changeLabel() {
         
-        let pickerDate = sender.date
+        let pickerDate = calendarDate.date
         let calendar = Calendar.current
-        
         let timezoneValue = Int(newTimeTextField.text ?? "") ?? 0
         let endDate = calendar.date(byAdding: .hour, value: timezoneValue, to: pickerDate)
         
         let dateFormatter = DateFormatter()
         dateFormatter.dateFormat = "dd-MM-yyyy 'Time' HH:mm:ss"
         dateLabel.text = dateFormatter.string(from: endDate!)
-        
     }
 }
