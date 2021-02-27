d53748ed9400af9ced6a8e3ed018bf3284ce5b2d
Tatiana <73175008+TatyanaV24@users.noreply.github.com>
2021-02-27 17:49:23 +0300

    Добавила условие валидации

--- Additional-tasks-for-module-8/Additional-tasks-for-module-8/CalendarViewController.swift
+++ Additional-tasks-for-module-8/Additional-tasks-for-module-8/CalendarViewController.swift
@@ -15,14 +15,17 @@ class CalendarViewController: UIViewController {
     @IBOutlet weak var dateLabel: UILabel!
     
     @IBAction func changeLabel() {
-        
-        let pickerDate = calendarDate.date
-        let calendar = Calendar.current
+        let timeZone = -12...14
         let timezoneValue = Int(newTimeTextField.text ?? "") ?? 0
-        let endDate = calendar.date(byAdding: .hour, value: timezoneValue, to: pickerDate)
-        
-        let dateFormatter = DateFormatter()
-        dateFormatter.dateFormat = "dd-MM-yyyy 'Time' HH:mm:ss"
-        dateLabel.text = dateFormatter.string(from: endDate!)
+        if timeZone.contains(timezoneValue) {
+            let pickerDate = calendarDate.date
+            let calendar = Calendar.current
+            let endDate = calendar.date(byAdding: .hour, value: timezoneValue, to: pickerDate)
+            let dateFormatter = DateFormatter()
+            dateFormatter.dateFormat = "dd-MM-yyyy 'Time' HH:mm:ss"
+            dateLabel.text = dateFormatter.string(from: endDate!)
+        } else {
+            dateLabel.text = "Указанного часового пояса не существует"
+        }
     }
 }
