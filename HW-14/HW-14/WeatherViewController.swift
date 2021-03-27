7aec8278980b74ea5a01059bb1f7ccb8cea607ad
Tatiana <73175008+TatyanaV24@users.noreply.github.com>
2021-03-27 21:54:40 +0300

    #2

--- HW-14/HW-14/WeatherViewController.swift
+++ HW-14/HW-14/WeatherViewController.swift
@@ -7,6 +7,7 @@
 
 import UIKit
 import Alamofire
+import RealmSwift
 
 class WeatherViewController: UIViewController {
 
@@ -18,14 +19,14 @@ class WeatherViewController: UIViewController {
     @IBOutlet weak var alamTableView: UITableView!
     
     var weatherDate = WeatherData()
-    var list = ListW()
-    var mode:[FeatherData] = []
+//    var list = ListW()
+//    var mode:[FeatherData] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        alamTableView.delegate = self
-        alamTableView.dataSource = self
+//        alamTableView.delegate = self
+//        alamTableView.dataSource = self
         
         AF.request ("http://api.openweathermap.org/data/2.5/weather?q=Moscow&units=metric&appid=ea5e2d3c2f5e9ec322593ff4b368cafc").responseDecodable(of: WeatherData.self) { response in
             print(response)
@@ -40,28 +41,28 @@ class WeatherViewController: UIViewController {
             }
         }
         
-        AF.request ("http://api.openweathermap.org/data/2.5/forecast?q=Moscow&units=metric&appid=ea5e2d3c2f5e9ec322593ff4b368cafc").responseDecodable(of: ListW.self) { response in
-            print(response)
-            
-            if let result = response.value {
-                let json = result
-                print(json)
-                DispatchQueue.main.async {
-                    var uniqueDates: [String] = []
-                    let dateFormat = DateFormatter()
-                    dateFormat.dateFormat = "MM/dd/yyyy"
-                    json.list.forEach { (data) in
-                        let formattedDate = dateFormat.string(from: Date(timeIntervalSince1970: TimeInterval(data.dt)))
-                        
-                        if !uniqueDates.contains(formattedDate) {
-                            uniqueDates.append(formattedDate)
-                            self.mode.append(data)
-                        }
-                        self.alamTableView.reloadData()
-                    }
-                }
-            }
-        }
+//        AF.request ("http://api.openweathermap.org/data/2.5/forecast?q=Moscow&units=metric&appid=ea5e2d3c2f5e9ec322593ff4b368cafc").responseDecodable(of: ListW.self) { response in
+//            print(response)
+//
+//            if let result = response.value {
+//                let json = result
+//                print(json)
+//                DispatchQueue.main.async {
+//                    var uniqueDates: [String] = []
+//                    let dateFormat = DateFormatter()
+//                    dateFormat.dateFormat = "MM/dd/yyyy"
+//                    json.list.forEach { (data) in
+//                        let formattedDate = dateFormat.string(from: Date(timeIntervalSince1970: TimeInterval(data.dt)))
+//
+//                        if !uniqueDates.contains(formattedDate) {
+//                            uniqueDates.append(formattedDate)
+//                            self.mode.append(data)
+//                        }
+//                        self.alamTableView.reloadData()
+//                    }
+//                }
+//            }
+//        }
     }
     
     func updateView() {
@@ -73,28 +74,28 @@ class WeatherViewController: UIViewController {
     }
 }
 
-extension WeatherViewController: UITableViewDataSource, UITableViewDelegate {
-    
-    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        return 126
-    }
-    
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return mode.count
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        
-        let cell = tableView.dequeueReusableCell(withIdentifier: "AlamofireTableViewCell", for: indexPath) as! AlamofireTableViewCell
-        let opt = mode[indexPath.row]
-        let date = Date(timeIntervalSince1970: TimeInterval(opt.dt))
-        let localDateFormat = DateFormatter()
-        localDateFormat.dateFormat = "dd.MM"
-        cell.dateLabel.text = localDateFormat.string(from: date)
-        cell.averTempLabel.text = opt.main.temp.description + "ºC"
-        cell.minTempLabel.text = opt.main.temp_min.description + "ºC"
-        cell.maxTempLabel.text = opt.main.temp_max.description + "ºC"
-        return cell
-    }
-}
+//extension WeatherViewController: UITableViewDataSource, UITableViewDelegate {
+//
+//    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+//        return 126
+//    }
+//
+//    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+//        return mode.count
+//    }
+//
+//    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+//
+//        let cell = tableView.dequeueReusableCell(withIdentifier: "AlamofireTableViewCell", for: indexPath) as! AlamofireTableViewCell
+//        let opt = mode[indexPath.row]
+//        let date = Date(timeIntervalSince1970: TimeInterval(opt.dt))
+//        let localDateFormat = DateFormatter()
+//        localDateFormat.dateFormat = "dd.MM"
+//        cell.dateLabel.text = localDateFormat.string(from: date)
+//        cell.averTempLabel.text = opt.main.temp.description + "ºC"
+//        cell.minTempLabel.text = opt.main.temp_min.description + "ºC"
+//        cell.maxTempLabel.text = opt.main.temp_max.description + "ºC"
+//        return cell
+//    }
+//}
 
